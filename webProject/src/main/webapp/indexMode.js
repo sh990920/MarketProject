23f247ea330ea3f1615802d316263d3678863c46
sh990920 <sh990920@gmail.com>
2022-09-28 23:30:55 +0900

    up_date

---
+++ webProject/src/main/webapp/indexMode.js
@@ -0,0 +1,42 @@
+//다크모드인지 데이모드인지 확인 변수
+var check = true;
+//버튼 변수
+var btn_mode = document.getElementById("btn_mode");
+//a태그 변수(querySelectorAll로 적용이 안됨)
+var a_tag1 = document.getElementById("a_tag1");
+var a_tag2 = document.getElementById("a_tag2");
+var a_tag3 = document.getElementById("a_tag3");
+//h2태그 변수(querySelectorAll로 적용이 안됨)
+var tx_h2_1 = document.getElementById("tx_h2_1");
+var tx_h2_2 = document.getElementById("tx_h2_2");
+//처음 css설정
+btn_mode.value = "다크모드로 보기";
+btn_mode.style.color = "white";
+btn_mode.style.backgroundColor = "black";
+//버튼이 눌렸을 때 실행할 메서드
+function mode() {
+	
+	if (check == true) { //다크모드
+		document.body.style.backgroundColor = 'black';
+		tx_h2_1.style.color = "white";
+		tx_h2_2.style.color = "white";
+		a_tag1.style.color = "white";
+		a_tag2.style.color = "white";
+		a_tag3.style.color = "white";
+		btn_mode.value = "데이모드로 보기";
+		btn_mode.style.color = "black";
+		btn_mode.style.backgroundColor = "white";
+		check = false;
+	} else { //데이모드
+		document.body.style.backgroundColor = 'white';
+		tx_h2_1.style.color = "black";
+		tx_h2_2.style.color = "black";
+		a_tag1.style.color = "black";
+		a_tag2.style.color = "black";
+		a_tag3.style.color = "black";
+		btn_mode.value = "다크모드로 보기";
+		btn_mode.style.color = "white";
+		btn_mode.style.backgroundColor = "black";
+		check = true;
+	}
+}
